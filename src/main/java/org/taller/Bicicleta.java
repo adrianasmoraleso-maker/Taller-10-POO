380fd9276b1fd6f82934612c6260a5252f78de5d
Adriana Morales <adrianasmoraleso@gmail.com>
2026-05-31 12:29:20 -0500

    chore/Ejercicio3: Uso incorrecto de sobrescritura: Se modificó la Clase Estudiante cambiando la firma del método, y se modificó la clase Bicicleta, eliminando la anotación @Override + Explicaciones en comentarios dentro del código

--- src/main/java/org/taller/Bicicleta.java
+++ src/main/java/org/taller/Bicicleta.java
@@ -2,9 +2,18 @@ package org.taller;
 
 public class Bicicleta extends Vehiculo {
 
-    @Override
+    //Ejercicio3: Sobrescritura de un método sin usar @Override
     public void moverse() {
 
         System.out.println("La bicicleta se mueve pedaleando");
     }
 }
+
+/* EXPLICACIÓN:
+Aunque un método puede sobrescribirse sin utilizar la anotación "@Override",
+esta práctica no es recomendable; pues la anotación permite al compilador verificar
+que el método realmente está sobrescribiendo uno existente en la superclase.
+Sin ella, errores tipográficos o cambios accidentales en la firma del método
+pueden pasar desapercibidos, generando comportamientos inesperados y dificultando
+el mantenimiento del código.
+*/
--- src/main/java/org/taller/Estudiante.java
+++ src/main/java/org/taller/Estudiante.java
@@ -11,9 +11,29 @@ public class Estudiante extends Persona {
         this.carrera = carrera;
     }
 
-    // Sobrescribe el comportamiento heredado para adaptarlo a un estudiante
+//Ejercicio3: Sobrescritura del método de una clase derivada cambiando su firma
+    @Override
+    public void presentarse(String mensaje) {
+        System.out.println(mensaje + " Soy estudiante de " + carrera);
+
+    }
+
+}
+
+/* EXPLICACIÓN:
+ La sobrescritura requiere que el método de la clase derivada tenga exactamente la misma firma
+ que el método definido en la clase base. Al agregar un parámetro al método "presentarse", la firma
+ cambia y deja de ser una sobrescritura válida. Como consecuencia, el compilador genera un error al
+ detectar que el método anotado con `@Override` no coincide con ningún método de la superclase
+ */
+
+//-----------------------------------------------------------------------------------------------------
+/*
+// Sobrescribe el comportamiento heredado para adaptarlo a un estudiante
     @Override
     public void presentarse() {
         System.out.println("Hola, soy estudiante de " + carrera + " y me llamo " + nombre + ".");
     }
 }
+*/
+
